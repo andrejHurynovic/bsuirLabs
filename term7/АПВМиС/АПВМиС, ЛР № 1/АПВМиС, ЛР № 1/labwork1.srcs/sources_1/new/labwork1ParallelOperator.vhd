5d7b8d9ff156371362bd43504014baae393d6993
Hurynovič Andrej <dpkdpk10@icloud.com>
2022-10-04 01:53:35 +0300

    АПВМиС, ЛР № 1

---
+++ term7/АПВМиС/АПВМиС, ЛР № 1/АПВМиС, ЛР № 1/labwork1.srcs/sources_1/new/labwork1ParallelOperator.vhd
@@ -0,0 +1,76 @@
+----------------------------------------------------------------------------------
+-- Company 
+-- Engineer 
+-- 
+-- Create Date 28.09.2022 231850
+-- Design Name 
+-- Module Name labwork1ParallelOperator - Behavioral
+-- Project Name 
+-- Target Devices 
+-- Tool Versions 
+-- Description 
+-- 
+-- Dependencies 
+-- 
+-- Revision
+-- Revision 0.01 - File Created
+-- Additional Comments
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity labwork1ParallelOperator is
+Port (
+      aIn, bIn, cIn, dIn: in std_logic;
+      notBI: in std_logic;
+      notRBI: in std_logic;
+      notLT: in std_logic;
+      
+      notRBO: out std_logic;
+      aOut, bOut, cOut, dOut, eOut, fOut, gOut: out std_logic
+      );
+--  Port ( );
+end labwork1ParallelOperator;
+
+architecture Behavioral of labwork1ParallelOperator is
+signal temp: std_logic_vector (7 downto 0);
+signal LT, pass: std_logic;
+
+
+begin
+
+temp(0) <= not aIn when pass = '1' else '0';
+temp(1) <= not(temp(0));
+temp(2) <= not bIn when pass = '1' else '0';
+temp(3) <= not(temp(2));
+temp(4) <= not cIn when pass = '1' else '0';
+temp(5) <= not(temp(4));
+temp(6) <= not dIn when pass = '1' else '0';
+temp(7) <= not(temp(6));
+
+
+LT <= not notLT when notBI = '1' else '0';
+notRBO <= '0' when notBI = '0' else '1' when LT = '1' else not(not(aIn or bIn or cIn or dIn) and (not notRBI));
+pass <= notBI and not(not(aIn or bIn or cIn or dIn) and (not notRBI));
+
+aOut <= not((temp(3) and temp(7)) or (temp(0) and temp(5)) or (temp(1) and temp(2) and temp(4) and temp(6))) or LT;
+bOut <= not((temp(3) and temp(7)) or (temp(1) and temp(2) and temp(5)) or (temp(0) and temp(3) and temp(5))) or LT;
+cOut <= not((temp(5) and temp(7)) or (temp(0) and temp(3) and temp(4))) or LT;
+dOut <= not((temp(1) and temp(2) and temp(4)) or (temp(0) and temp(2) and temp(5)) or (temp(1) and temp(3) and temp(5))) or LT;
+eOut <= not(temp(1) or (temp(2) and temp(5))) or LT;
+fOut <= not((temp(1) and temp(3)) or (temp(3) and temp(4)) or (temp(1) and temp(4) and temp(6))) or LT;
+gOut <= not((temp(1) and temp(3) and temp(5)) or (temp(2) and temp(4) and temp(6) and notLT)) or LT;
+ 
+end Behavioral;
---
+++ term7/АПВМиС/АПВМиС, ЛР № 1/АПВМиС, ЛР № 1/labwork1.srcs/sources_1/new/labwork1SerialOperator.vhd
@@ -0,0 +1,94 @@
+----------------------------------------------------------------------------------
+-- Company:
+-- Engineer:
+--
+-- Create Date: 28.09.2022 23:18:50
+-- Design Name:
+-- Module Name: labwork1SerialOperator - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versi1s:
+-- Descripti1:
+--
+-- Dependencies:
+--
+-- Revisi1:
+-- Revisi1 0.01 - File Created
+-- Additi1al Comments:
+--
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declarati1 if using
+-- arithmetic functi1s with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declarati1 if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComp1ents.all;
+
+entity labwork1SerialOperator is
+Port (
+      aIn, bIn, cIn, dIn: in std_logic;
+      notBI: in std_logic;
+      notRBI: in std_logic;
+      notLT: in std_logic;
+      
+      notRBO: out std_logic;
+      aOut, bOut, cOut, dOut, eOut, fOut, gOut: out std_logic
+      );
+--  Port ( );
+end labwork1SerialOperator;
+
+architecture Behavioral of labwork1SerialOperator is
+
+begin
+
+process(aIn, bIn, cIn, dIn, notBI, notRBI, notLT)
+
+begin
+if(notBI = '0') then aOut <= '0'; bOut <= '0'; cOut <= '0'; dOut <= '0'; eOut <= '0'; fOut <= '0'; gOut <= '0'; notRBO <= '0';
+elsif (notLT = '0') then aOut <= '1'; bOut <= '1'; cOut <= '1'; dOut <= '1'; eOut <= '1'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+elsif (notLT = '1' and notRBI = '0' and dIn = '0' and cIn = '0' and bIn = '0' and aIn = '0' and notBI = '0') then aOut <= '0'; bOut <= '0'; cOut <= '0'; dOut <= '0'; eOut <= '0'; fOut <= '0'; gOut <= '0'; notRBO <= '0';
+--0
+elsif (notLT = '1' and notRBI = '1' and dIn = '0' and cIn = '0' and bIn = '0' and aIn = '0') then aOut <= '1'; bOut <= '1'; cOut <= '1'; dOut <= '1'; eOut <= '1'; fOut <= '1'; gOut <= '0'; notRBO <= '1';
+--1
+elsif (notLT = '1' and dIn = '0' and cIn = '0' and bIn = '0' and aIn = '1') then aOut <= '0'; bOut <= '1'; cOut <= '1'; dOut <= '0'; eOut <= '0'; fOut <= '0'; gOut <= '0'; notRBO <= '1';
+--2
+elsif (notLT = '1' and dIn = '0' and cIn = '0' and bIn = '1' and aIn = '0') then aOut <= '1'; bOut <= '1'; cOut <= '0'; dOut <= '1'; eOut <= '1'; fOut <= '0'; gOut <= '1'; notRBO <= '1';
+--3
+elsif (notLT = '1' and dIn = '0' and cIn = '0' and bIn = '1' and aIn = '1') then aOut <= '1'; bOut <= '1'; cOut <= '1'; dOut <= '1'; eOut <= '0'; fOut <= '0'; gOut <= '1'; notRBO <= '1';
+--4
+elsif (notLT = '1' and dIn = '0' and cIn = '1' and bIn = '0' and aIn = '0') then aOut <= '0'; bOut <= '1'; cOut <= '1'; dOut <= '0'; eOut <= '0'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--5
+elsif (notLT = '1' and dIn = '0' and cIn = '1' and bIn = '0' and aIn = '1') then aOut <= '1'; bOut <= '0'; cOut <= '1'; dOut <= '1'; eOut <= '0'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--6
+elsif (notLT = '1' and dIn = '0' and cIn = '1' and bIn = '1' and aIn = '0') then aOut <= '0'; bOut <= '0'; cOut <= '1'; dOut <= '1'; eOut <= '1'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--7
+elsif (notLT = '1' and dIn = '0' and cIn = '1' and bIn = '1' and aIn = '1') then aOut <= '1'; bOut <= '1'; cOut <= '1'; dOut <= '0'; eOut <= '0'; fOut <= '0'; gOut <= '0'; notRBO <= '1';
+--8
+elsif (notLT = '1' and dIn = '1' and cIn = '0' and bIn = '0' and aIn = '0') then aOut <= '1'; bOut <= '1'; cOut <= '1'; dOut <= '1'; eOut <= '1'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--9
+elsif (notLT = '1' and dIn = '1' and cIn = '0' and bIn = '0' and aIn = '1') then aOut <= '1'; bOut <= '1'; cOut <= '1'; dOut <= '0'; eOut <= '0'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--10
+elsif (notLT = '1' and dIn = '1' and cIn = '0' and bIn = '1' and aIn = '0') then aOut <= '0'; bOut <= '0'; cOut <= '0'; dOut <= '1'; eOut <= '1'; fOut <= '0'; gOut <= '1'; notRBO <= '1';
+--11
+elsif (notLT = '1' and dIn = '1' and cIn = '0' and bIn = '1' and aIn = '1') then aOut <= '0'; bOut <= '0'; cOut <= '1'; dOut <= '1'; eOut <= '0'; fOut <= '0'; gOut <= '1'; notRBO <= '1';
+--12
+elsif (notLT = '1' and dIn = '1' and cIn = '1' and bIn = '0' and aIn = '0') then aOut <= '0'; bOut <= '1'; cOut <= '0'; dOut <= '0'; eOut <= '0'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--13
+elsif (notLT = '1' and dIn = '1' and cIn = '1' and bIn = '0' and aIn = '1') then aOut <= '1'; bOut <= '0'; cOut <= '0'; dOut <= '1'; eOut <= '0'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--14
+elsif (notLT = '1' and dIn = '1' and cIn = '1' and bIn = '1' and aIn = '0') then aOut <= '0'; bOut <= '0'; cOut <= '0'; dOut <= '1'; eOut <= '1'; fOut <= '1'; gOut <= '1'; notRBO <= '1';
+--15
+elsif (notLT = '1' and dIn = '1' and cIn = '1' and bIn = '1' and aIn = '1') then aOut <= '0'; bOut <= '0'; cOut <= '0'; dOut <= '0'; eOut <= '0'; fOut <= '0'; gOut <= '0'; notRBO <= '1';
+
+end if;
+
+end process;
+
+end Behavioral;
